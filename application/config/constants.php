3f89401a9bfc9da61b35b4208937adbadac649b0
mool-singh <mool30699@gmail.com>
2024-07-06 11:48:21 +0530

    merge

--- application/config/constants.php
+++ application/config/constants.php
@@ -85,11 +85,11 @@ defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automat
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
 define('SITE_NAME','Brand Metrics');
-define('ZOHO_ORG_ID','60030695246');
+define('ZOHO_ORG_ID','');
 define('ZOHO_ACCESS_TOKEN','Brand Metrics');
 define('ZOHO_REFRESH_TOKEN','Brand Metrics');
-define('ZOHO_CLIENT_ID','1000.AD5K0C20OB104PVWHR1J4B5V68RIYZ');
-define('ZOHO_CLIENT_SECRET','5761b33615287866f3bb93c10abf2b476d0dada3a0');
+define('ZOHO_CLIENT_ID','');
+define('ZOHO_CLIENT_SECRET','');
 define('ZOHO_REDIRECT_URI','capture-token');
 define('ZOHO_ACCOUNT_URL','https://accounts.zoho.in/');
 
